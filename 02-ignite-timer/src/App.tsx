dabc0e44fc96fba09e6432706c64b227dc3887ec
= <alexandressoares383@gmail.com>
2024-02-08 22:04:48 -0300

    feat: configurando eslint

---
+++ 02-ignite-timer/src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+import { Button } from './components/Button'
+import { GlobalStyle } from './global'
+
+export const App = () => {
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+        <Button />
+
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  )
+}
