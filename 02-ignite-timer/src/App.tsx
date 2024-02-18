b87309f3b8c8bf3d4add4793fee9e546e14a9201
= <alexandressoares383@gmail.com>
2024-02-17 21:52:58 -0300

    feat: criando reducer de ciclos

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,11 +1,9 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
+import { Router } from './router'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './global'
-
-import { Router } from './router'
-
 import { CyclesContextProvider } from './contexts/CycleContext'
 
 export const App = () => {
