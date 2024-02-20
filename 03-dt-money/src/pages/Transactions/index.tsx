8fb2da125b8f609a67a22e1085a6346a9307f9cb
= <alexandressoares383@gmail.com>
2024-02-20 12:32:28 -0300

    feat: tabela de transacoes

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,32 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import { PricingHighLight, TransactionsContainer, TransactionsTable } from './styles'
+
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de sites</td>
+              <td><PricingHighLight variant="income">R$ 12.000,00</PricingHighLight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de sites</td>
+              <td><PricingHighLight variant="outcome">R$ -62,00</PricingHighLight></td>
+              <td>Venda</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
