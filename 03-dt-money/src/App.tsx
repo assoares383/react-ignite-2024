115fb6046187bd66b2da3553617e52fcfab3edee
= <alexandressoares383@gmail.com>
2024-02-21 18:43:44 -0300

    feat: criando contexto de transações

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 
+import { TransactionsProvider } from './contexts/TransactionsContext'
 import { Transactions } from './pages/Transactions';
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Transactions />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
