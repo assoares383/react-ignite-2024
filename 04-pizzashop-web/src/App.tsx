a64abe5cd3fb644dda52a68de29d8fb5954023f4
= <alexandressoares383@gmail.com>
2024-12-27 18:56:59 -0300

    feat: add react helmet async

--- 04-pizzashop-web/src/App.tsx
+++ 04-pizzashop-web/src/App.tsx
@@ -1,7 +1,15 @@
+import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import { router } from "./routes";
+
 export function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router}/>
+    </HelmetProvider>
+  );
 }
