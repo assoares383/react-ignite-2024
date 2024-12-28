ee3fccc5399cfe3863f459dce39d3dadea14936c
= <alexandressoares383@gmail.com>
2024-12-28 18:13:14 -0300

    feat: add react-hook-form zod @hookform/resolvers

--- 04-pizzashop-web/src/pages/auth/sign-in.tsx
+++ 04-pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,10 +1,54 @@
 import { Helmet } from "react-helmet-async";
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from "../../components/ui/button";
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
+
+const signInForm = z.object({
+  email: z.string().email()
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting }
+   } = useForm<SignInForm>();
+
+   const handleSignIn = async (data: SignInForm) => {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 20000));
+   }
+
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
