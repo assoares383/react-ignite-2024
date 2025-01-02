9e595af7285cf85c0e6ec8e1a0739b0fab2f4fe0
= <alexandressoares383@gmail.com>
2025-01-02 18:46:46 -0300

    feat: modal de atualizacao do perfil

--- 04-pizzashop-web/src/components/account-menu.tsx
+++ 04-pizzashop-web/src/components/account-menu.tsx
@@ -13,6 +13,8 @@ import {
 
 import { getManagedRestaurant } from "../api/get-managed-restaurant";
 import { getProfile } from "../api/get-profile";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StroreProfileDialog } from "./store-profile-dialog";
 
 
 export function AccountMenu() {
@@ -27,42 +29,48 @@ export function AccountMenu() {
   })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex items-center gap-2 select-none">
-          {isLoadingManagedRestaurant ? (<Skeleton className="h-4 w-40" />) : managedRestaurant?.name}
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center gap-2 select-none">
+            {isLoadingManagedRestaurant ? (<Skeleton className="h-4 w-40" />) : managedRestaurant?.name}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {
-            isLoadingProfile ? (
-              <div className="space-y-1 5">
-                <Skeleton className="h-4 w-32" />
-                <Skeleton className="h-4 w-24" />
-              </div>
-            ) : (
-              <>
-                <span>{profile?.name}</span>
-                <span className="text-xs font-normal text-muted-foreground">
-                  {profile?.email}
-                </span>
-              </>
-            )
-          }
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="m-4 h-4 mr-2" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="m-4 h-4 mr-2" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {
+              isLoadingProfile ? (
+                <div className="space-y-1 5">
+                  <Skeleton className="h-4 w-32" />
+                  <Skeleton className="h-4 w-24" />
+                </div>
+              ) : (
+                <>
+                  <span>{profile?.name}</span>
+                  <span className="text-xs font-normal text-muted-foreground">
+                    {profile?.email}
+                  </span>
+                </>
+              )
+            }
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="m-4 h-4 mr-2" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut className="m-4 h-4 mr-2" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StroreProfileDialog />
+    </Dialog>
   )
 }
