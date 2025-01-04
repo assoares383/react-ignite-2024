329d4665ace2d26e80a494780175450118da63a0
= <alexandressoares383@gmail.com>
2025-01-04 13:13:51 -0300

    feat: detalhes do pedido

--- 04-pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ 04-pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 import { ArrowRight, Search, X } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import { TableCell, TableRow } from "../../../components/ui/table";
@@ -5,10 +10,6 @@ import { Dialog, DialogTrigger } from "../../../components/ui/dialog";
 
 import { OrderDetails } from "./order-details";
 import { OrderStatus } from "../../../components/order-status";
-
-import { formatDistanceToNow } from 'date-fns';
-import { ptBR } from 'date-fns/locale';
-
 export interface OrderTableRowProps {
   order: {
     orderId: string
@@ -20,10 +21,12 @@ export interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
           <DialogTrigger asChild>
             <Button variant="outline" size="xs">
               <Search className="h-3 w-3" />
@@ -31,7 +34,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </Button>
           </DialogTrigger>
 
-          <OrderDetails />
+          <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
@@ -50,7 +53,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         { order.customerName }
       </TableCell>
       <TableCell className="font-medium">
-        { order.total.toLocaleString('pt-BR', {
+        { (order.total / 100).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
         })}
