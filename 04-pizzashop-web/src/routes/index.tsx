5e41d0442b9fb3ce76a04103ec00b059cb06638e
= <alexandressoares383@gmail.com>
2025-01-08 22:55:22 -0300

    feat: erros e pagina 404

--- 04-pizzashop-web/src/routes/index.tsx
+++ 04-pizzashop-web/src/routes/index.tsx
@@ -8,12 +8,13 @@ import { Orders } from "../pages/app/orders/orders";
 import { SignIn } from "../pages/auth/sign-in";
 import { SignUp } from "../pages/auth/sign-up";
 import { NotFound } from "../pages/404";
+import { Error } from "../pages/error";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -38,5 +39,9 @@ export const router = createBrowserRouter([
         element: <SignUp />
       }
     ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
   }
 ])
