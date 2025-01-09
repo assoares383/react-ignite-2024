c249a91469326cb378b5d664bafad2144256be8a
= <alexandressoares383@gmail.com>
2025-01-08 23:35:38 -0300

    feat: setup do ambiente de testes

--- 04-pizzashop-web/vite.config.ts
+++ 04-pizzashop-web/vite.config.ts
@@ -1,13 +1,16 @@
 import path from "node:path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vitest/config'
+
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./src/__tests__/setup.ts'],
+    environment: 'happy-dom'
   },
 })
