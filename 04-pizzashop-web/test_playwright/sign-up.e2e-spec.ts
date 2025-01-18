e00942aef073b32349b48723635dade8cfc94b23
= <alexandressoares383@gmail.com>
2025-01-18 17:15:36 -0300

    test: sign up

---
+++ 04-pizzashop-web/test_playwright/sign-up.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test';
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('1234567890')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso')
+
+  expect(toast).toBeVisible()
+});
+
+// test('sign in with wrong credentiaals', async ({ page }) => {
+//   await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+//   await page.getByLabel('Seu e-mail').fill('wrong@example.com')
+//   await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+//   const toast = page.getByText('Credenciais invalidas.')
+
+//   expect(toast).toBeVisible()
+// });
+
+test('navigate to new login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('sign-in')
+});
