e53d427ee44a9aaac33b93cff774b7b9220805f8
= <alexandressoares383@gmail.com>
2025-01-23 09:21:22 -0300

    feat: estrutura da home

--- 05.ignite-shop/src/pages/index.tsx
+++ 05.ignite-shop/src/pages/index.tsx
@@ -1,22 +1,30 @@
-import { styled } from "@/styles";
+import Image from 'next/image'
 
-const Button = styled("button", {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  span: {
-    fontWeight: 'bold'
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
 
 export default function Home() {
   return (
-    <Button>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+
   );
 }
