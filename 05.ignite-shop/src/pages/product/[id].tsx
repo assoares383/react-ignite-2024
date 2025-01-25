7242af7a9d28d1edeb8efcb26ff59d1fe8adc9b6
= <alexandressoares383@gmail.com>
2025-01-25 17:28:47 -0300

    feat: estrutura da pagina de produto

--- 05.ignite-shop/src/pages/product/[id].tsx
+++ 05.ignite-shop/src/pages/product/[id].tsx
@@ -1,12 +1,27 @@
 import { useRouter } from 'next/router';
 
+import { ImageContainer, ProductContainer } from '@/styles/pages/product';
+import Image from 'next/image';
+
+import Camiseta1 from '../../assets/camiseta1.jpg';
+import { ProductDetails } from '../../styles/pages/product';
 
 export default function Product() {
-  const { query } = useRouter();
-
   return (
-    <>
-      <h1>Produto: {JSON.stringify(query)}!</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={Camiseta1} alt='' />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta 1</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis optio non fugit doloribus ab exercitationem? Ipsam, maxime dolor? Fuga, id. Assumenda non nobis error natus? Explicabo nisi praesentium suscipit laboriosam.</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
