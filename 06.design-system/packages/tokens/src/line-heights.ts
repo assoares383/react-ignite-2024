f5a590dabbad1aab8addcb497d2ca33645122624
= <alexandressoares383@gmail.com>
2025-01-26 21:57:58 -0300

    chore: add eslint

--- 06.design-system/packages/tokens/src/line-heights.ts
+++ 06.design-system/packages/tokens/src/line-heights.ts
@@ -1,6 +1,6 @@
 export const lineHeights = {
-    shorter: '125%',
-    short: '148%',
-    base: '160%',
-    tall: '180%',
+  shorter: '125%',
+  short: '148%',
+  base: '160%',
+  tall: '180%',
 }
