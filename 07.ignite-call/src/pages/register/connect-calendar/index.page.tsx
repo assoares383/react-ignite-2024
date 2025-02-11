fe5f82bed267095da4ec49e72185d3518fd4fdec
= <alexandressoares383@gmail.com>
2025-02-11 08:38:23 -0300

    feat: configurando o next auth

--- 07.ignite-call/src/pages/register/connect-calendar/index.page.tsx
+++ 07.ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -2,6 +2,7 @@ import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
 
 import { ArrowRight } from "phosphor-react";
 
+import { signIn } from "next-auth/react";
 import { ConnectBox, ConnectItem, Container, Header } from "./styles";
 
 export default function Register() {
@@ -22,7 +23,7 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
             <Text>Google Calendar</Text>
-            <Button variant="secondary" size="sm">
+            <Button variant="secondary" size="sm" onClick={() => signIn('google')}>
                 Conectar
                 <ArrowRight />
             </Button>
