3819c8180a37cec29cad617359adffb15054971d
= <alexandressoares383@gmail.com>
2025-02-20 21:02:29 -0300

    feat: criando o usuario no adapter

--- 07.ignite-call/src/lib/auth/prisma-adapter.ts
+++ 07.ignite-call/src/lib/auth/prisma-adapter.ts
@@ -1,7 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { Adapter } from 'next-auth/adapters'
 import { prisma } from '../prisma'
 
-export function PrismaAdapter(): Adapter {
+export function PrismaAdapter(req: NextApiRequest, res: NextApiResponse): Adapter {
   return {
     async createUser() { },
 
