a961a7bae0db7a83dd899ccd5133a6689a3c4dba
= <alexandressoares383@gmail.com>
2025-03-13 10:47:23 -0300

    feat: pagina de agendamento

---
+++ 07.ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,57 @@
+import { prisma } from "@/lib/prisma";
+import { Avatar, Heading, Text } from "@ignite-ui/react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Container, UseHeader } from "./styles";
+
+interface ScheduleProps {
+  user: {
+    name: string,
+    bio: string,
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UseHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UseHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths:  GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+
+  if (!user)  {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url
+      }
+    },
+    revalidate: 60 * 60 * 24 // 1dia
+  }
+}
