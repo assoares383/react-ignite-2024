484b3db8192d4818b0b56ea37264316f272b3411
= <alexandressoares383@gmail.com>
2025-03-13 15:44:05 -0300

    feat: estrutura visual do calendario

--- 07.ignite-call/src/pages/schedule/[username]/index.page.tsx
+++ 07.ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { Avatar, Heading, Text } from "@ignite-ui/react";
 import { GetStaticPaths, GetStaticProps } from "next";
+import { ScheduleForm } from "./ScheduleForm";
 import { Container, UseHeader } from "./styles";
 
 interface ScheduleProps {
@@ -19,6 +20,8 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UseHeader>
+
+      <ScheduleForm />
     </Container>
   )
 }
