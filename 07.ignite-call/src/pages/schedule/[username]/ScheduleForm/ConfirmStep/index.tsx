b50e544c5303d5eacaf2fc2606fa36b4f63ee42b
= <alexandressoares383@gmail.com>
2025-03-18 11:19:55 -0300

    feat: formulario de confirmacao

--- 07.ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ 07.ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,12 +1,33 @@
-import { Button, Text, TextArea, TextInput } from '@ignite-ui/react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
 import { CalendarBlank, Clock } from "phosphor-react";
-import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
+
+const confirmFormSchema = z.object({
+  name: z.string().min(3, { message: 'O nome precisa ter pelo menos 3 caracteres'}),
+  email: z.string().email({ message: 'Digite um e-mail válido'}),
+  observations: z.string().nullable(),
+});
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>;
 
 export function ComfirmStep() {
-  function handleConfirmScheduling() {}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  });
+
+  function handleConfirmScheduling(data: ConfirmFormData) {
+    console.log(data)
+  }
 
   return (
-    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
@@ -20,23 +41,45 @@ export function ComfirmStep() {
 
       <label>
         <Text size="sm">Nome Completo</Text>
-        <TextInput placeholder='Seu nome' onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+        <TextInput
+          placeholder="Seu nome"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          crossOrigin={undefined}
+          {...register("name")}
+        />
+        {errors.name && (
+          <FormError>{errors.name.message}</FormError>
+        )}
       </label>
 
       <label>
         <Text size="sm">Endereco de e-mail</Text>
-        <TextInput placeholder='johndoe@example.com' onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+        <TextInput
+          placeholder="johndoe@example.com"
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          crossOrigin={undefined}
+          {...register("email")}
+        />
+        {errors.email && (
+          <FormError>{errors.email.message}</FormError>
+        )}
       </label>
 
       <label>
-        <Text size="sm">Observacoes</Text>
+        <Text size="sm" {...register("observations")}>
+          Observacoes
+        </Text>
         <TextArea />
       </label>
 
       <FormActions>
-        <Button type='button' variant="tertiary">Cancelar</Button>
-        <Button type='button'>Confirmar</Button>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="button" disabled={isSubmitting}>Confirmar</Button>
       </FormActions>
     </ConfirmForm>
-  )
+  );
 }
