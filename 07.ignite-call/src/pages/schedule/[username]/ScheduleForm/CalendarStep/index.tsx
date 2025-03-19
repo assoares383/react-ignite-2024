777e1e0835f830214168135e504ef00441e8ce18
= <alexandressoares383@gmail.com>
2025-03-19 13:42:29 -0300

    feat: carregando horarios disponiveis

--- 07.ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ 07.ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,21 +1,51 @@
 import { Calendar } from "@/components/Calendar";
+import { api } from "@/lib/axios";
 import dayjs from "dayjs";
-import { useState } from "react";
-import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
 
 export function CalendarStep() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  // const [availability, setAvailability] = useState(null)
+
+  const router = useRouter();
 
   const isDateSelected = !!selectedDate;
+  const username = String(router.query.username);
 
-  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : '';
-  const describedDate = selectedDate ? dayjs(selectedDate).format('DD[ de ]MMMM') : '';
+  const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : "";
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format("DD[ de ]MMMM")
+    : "";
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return;
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format("YYYY-MM-DD"),
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, [selectedDate, username]);
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
-      { isDateSelected && (
+      {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
             {weekDay}, <span>{describedDate}</span>
@@ -33,10 +63,9 @@ export function CalendarStep() {
             <TimePickerItem>16:00h</TimePickerItem>
             <TimePickerItem>17:00h</TimePickerItem>
             <TimePickerItem>18:00h</TimePickerItem>
-
           </TimePickerList>
         </TimePicker>
       )}
     </Container>
-  )
+  );
 }
